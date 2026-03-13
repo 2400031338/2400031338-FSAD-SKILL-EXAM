cde4307994669b689ccca9d61eade255c23a0afd
BALA TIRUPATAIAH <gorlabala7@gmail.com>
2026-03-13 09:14:11 +0530

    Remove skill folders

--- Skill_6/src/main/java/com/klu/library/LibraryApplication.java
+++
@@ -1,5 +0,0 @@
-package com.klu.library;
-
-public class LibraryApplication {
-
-}
--- Skill_9/src/main/java/com/klu/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klu.repository;
-
-import com.klu.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
